c29c443c3e644bcfe12989bbe1832cb054651357
hanielcedraz <30409702+hanielcedraz@users.noreply.github.com>
2019-02-18 13:10:47 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
+while getopts 'h' c;
+do
+  echo "help"
+  case $c in
+    h) ACTION=echo "#Use this script to change the trimmomatic path in "qc_trimmomatic-V.0.0.1" 
+        and put theses file into the bash"
 
-
-#Use this script to change the trimmomatic path in R and put theses file into the bash
 
 cat "qc_trimmomatic-V.0.0.1" | sed "s|XXX|$PWD|" > qc_trimmomatic-V.0.0.1.R
 
@@ -9,3 +13,6 @@ chmod +x qc_trimmomatic-V.0.0.1.R
 echo -e "\n"PATH=$PATH:$PWD >> ~/.bashrc
 bash
 
+ln -s qc_trimmomatic-V.0.0.1.R qc_trimmomatic
+
+mv qc_trimmomatic-V.0.0.1 .qc_trimmomatic-V.0.0.1
