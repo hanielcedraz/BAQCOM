94bcf29cc67361f6f4fb0ea46b630be5d45719b4
hanielcedraz <30409702+hanielcedraz@users.noreply.github.com>
2019-02-18 21:34:04 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,22 +2,29 @@ while getopts 'h' c;
 do
   echo "help"
   case $c in
-    h) ACTION=echo "#Use this script to change the trimmomatic path in "qc_trimmomatic-V.0.0.1" 
-        and put theses file into the bash"
+        h) ACTION= echo "#Use this script to change the trimmomatic path in "qc_trimmomatic-V.0.0.1" and put theses file into the bash"
 
+           exit
+           ;;
+  esac
+done
+shift $((OPTIND - 1))
+
+#Use this script to change the trimmomatic path in R and put theses file into the bash
 
 cat "qc_trimmomatic-V.0.0.1" | sed "s|XXX|$PWD|" > qc_trimmomatic-V.0.0.1.R
 
 chmod +x qc_trimmomatic-V.0.0.1.R
 
-grep "$PWD" > /dev/null
-if [ $? -eq 1 ] then
-  echo -e "\n"PATH=$PATH:$PWD >> ~/.bashrc
-  bash
+
+grep "$PWD" ~/.bashrc > /dev/null
+if [ $? -ne 0 ]; then
+  echo -e "\n"PATH=$PATH:$PWD >> ~/.bashrc;
+  bash;
   
   ln -s qc_trimmomatic-V.0.0.1.R qc_trimmomatic
-
+  ln -s mapping_STAR-V-0.0.1.R mapping_STAR
   mv qc_trimmomatic-V.0.0.1 .qc_trimmomatic-V.0.0.1
+  
 fi
-
 
