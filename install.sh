84bd54515e10375f1765f421ff3c8d2c568a8352
hanielcedraz <30409702+hanielcedraz@users.noreply.github.com>
2019-02-19 23:18:57 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,22 @@
 if [[ ( $@ == "--help") ||  $@ == "-h" ]];
     then
-       echo "#Use this script to change the trimmomatic path in ".qc_trimmomatic" and put theses file into the bash"
-       exit 0
+        echo "#Use this script to change the trimmomatic path in ".qc_trimmomatic" and put theses file into the bash"
+        exit 0
     else
-       echo "It is not a valid argment. Try ./install.sh -h or ./install.sh --help"
-
+        echo "It is not a valid argment. Try ./install.sh -h or ./install.sh --help"
+        stop
 fi
 
 grep "$PWD" ~/.bash_profile > /dev/null
-if [ $? -ne 0 ]; then
-  cat ".qc_trimmomatic" | sed "s|XXX|$PWD|" > qc_trimmomatic.R
-  echo -e "\nPATH=\$PATH:$PWD:\n" >> ~/.bash_profile;
-  source ~/.bash_profile;
-  chmod +x qc_trimmomatic.R mapping_STAR.R .install_packages.R create_samples.sh;
-  .install_packages.R;
-else
-  echo "It is already installed"
+if [ $? -ne 0 ]; 
+    then
+        cat ".qc_trimmomatic" | sed "s|XXX|$PWD|" > qc_trimmomatic.R
+        echo -e "\nPATH=\$PATH:$PWD:\n" >> ~/.bash_profile;
+        source ~/.bash_profile;
+        chmod +x qc_trimmomatic.R mapping_STAR.R .install_packages.R create_samples.sh;
+        .install_packages.R;
+    else
+        echo "It is already installed"
 fi
 
  
