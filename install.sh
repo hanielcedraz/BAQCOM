5ef29044e795df3eb951b5b5c9021295a798ea51
hanielcedraz <30409702+hanielcedraz@users.noreply.github.com>
2019-02-19 22:08:45 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ do
         h) ACTION= echo "#Use this script to change the trimmomatic path in ".qc_trimmomatic" and put theses file into the bash"
 
            exit
+  stop         
            ;;
   esac
 done
@@ -12,15 +13,18 @@ shift $((OPTIND - 1))
 
 #Use this script to change the trimmomatic path in R and put theses file into the bash
 
-chmod +x qc_trimmomatic.R mapping_STAR.R install_packages.R
+
 
 grep "$PWD" ~/.bash_profile > /dev/null
 if [ $? -ne 0 ]; then
   cat ".qc_trimmomatic" | sed "s|XXX|$PWD|" > qc_trimmomatic.R
-  chmod +x qc_trimmomatic.R
   echo -e "\nPATH=\$PATH:$PWD:\n" >> ~/.bash_profile;
   source ~/.bash_profile;
-  ./install_packages.R
+  
 else
   echo "It is already installed"
 fi
+
+chmod +x qc_trimmomatic.R mapping_STAR.R install_packages.R
+
+./install_packages.R
