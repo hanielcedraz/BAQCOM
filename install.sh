13f530eb3826587c41b1e351af1402e9de8824f1
hanielcedraz <30409702+hanielcedraz@users.noreply.github.com>
2019-02-19 23:23:20 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ if [[ ( $@ == "--help") ||  $@ == "-h" ]];
         exit 0
     else
         echo "It is not a valid argment. Try ./install.sh -h or ./install.sh --help"
-        stop
+        exit
 fi
 
 grep "$PWD" ~/.bash_profile > /dev/null
