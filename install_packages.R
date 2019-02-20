8d7e0340f6f35f043f94a438758cfc96ab6e9f88
hanielcedraz <30409702+hanielcedraz@users.noreply.github.com>
2019-02-19 21:58:20 -0300

    Add files via upload

---
+++ install_packages.R
@@ -0,0 +1,12 @@
+#!/usr/bin/env Rscript
+
+
+Install_And_Load <- function(packages) {
+    k <- packages[!(packages %in% installed.packages()[,'Package'])];
+    if(length(k))
+    {install.packages(k, repos='https://cran.rstudio.com/');}
+    
+    for(package_name in packages)
+    {library(package_name,character.only=TRUE, quietly = TRUE);}
+}
+Install_And_Load(c('optparse', 'parallel'))
