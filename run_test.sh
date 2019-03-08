b5313ef098482c9baab927a42c5b73552b6efead
hanielcedraz <30409702+hanielcedraz@users.noreply.github.com>
2019-03-08 09:59:30 -0300

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,10 +1,13 @@
 gunzip exemples/genome/Escherichia*
+echo -e "files extracted successfully\n"
 
 #create input_folder
 mkdir 00-Fastq
+echo -e "00-Fast1 created successfully\n"
 
 #moving files from exemples folder to 00-Fastq Folder
 cp exemples/SAMPLE* 00-Fastq/
+echoh -e "files moved successfully\n"
 
 #Creating samples.txt
 ./create_samples.sh
