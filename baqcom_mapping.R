5da290f9f6e965a970a210a431ded19d4c2c5e9e
hanielcedraz <30409702+hanielcedraz@users.noreply.github.com>
2019-03-11 10:36:32 -0300

    update to version 0.0.2

--- baqcom_mapping.R
+++ baqcom_mapping.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env Rscript
 
 #STAR
-#suppressPackageStartupMessages(library("tools"))
+suppressPackageStartupMessages(library("tools"))
 suppressPackageStartupMessages(library("parallel"))
 suppressPackageStartupMessages(library("optparse"))
 
@@ -19,14 +19,11 @@ option_list <- list(
     make_option(c("-b", "--mappingFolder"), type="character", default='02-mappingSTAR',
                 help="Directory where to store the mapping results [default %default]",
                 dest="mappingFolder"),
-    make_option(c('-m', '--genome'), type = 'character', default = 'index_STAR',
-                help = 'Folder that contains gtf file [default %default]',
-                dest = 'genomeDir'),
-    make_option(c("-t", "--mappingTargets"), type="character", default="genome_target.fa",
-                help="Path to a fasta file, to run mapping against [default %default]",
+    make_option(c("-t", "--mappingTargets"), type="character", default="mapping_targets.txt",
+                help="Path to a fasta file, or tab delimeted file with [target name]\t[target fasta]\t[target gtf, optional] to run mapping against [default %default]",
                 dest="mappingTarget"),
-    make_option(c("-g", "--gtfTargets"), type="character", default="gtf_file_target.gtf",
-                help="Path to a gtf file, to run mapping against [default %default]",
+    make_option(c("-g", "--gtfTargets"), type="character", default="gtf_targets.txt",
+                help="Path to a gtf file, or tab delimeted file with [target name]\t[target fasta]\t[target gtf] to run mapping against [default %default]",
                 dest="gtfTarget"),
     make_option(c("-p", "--processors"), type="integer", default=8,
                 help="number of processors to use [default %default]",
@@ -34,7 +31,7 @@ option_list <- list(
     make_option(c("-s", "--sjdboverhang"), type="integer", default=100,
                 help="Specifie the length of the genomic sequence around the annotated junction to be used in constructing the splice junctions database [default %default]",
                 dest="annoJunction"),
-    make_option(c("-e", "--unmappedFolder"), type="character", default="03-Unmapped",
+    make_option(c("-e", "--extractFolder"), type="character", default="03-Ummapped",
                 help="if extractUnmapped, and/or extractMapped is TRUE, save resulting fastq to this folder [default %default]",
                 dest="extractedFolder"),
     make_option(c("-z", "--readfilesCommand"), type = "character", default = "gunzip",
@@ -45,7 +42,7 @@ option_list <- list(
 # get command line options, if help option encountered print help and exit,
 # otherwise if options not found on command line then set defaults,
 opt <- parse_args(OptionParser(option_list = option_list, description =  paste('Authors: OLIVEIRA, H.C. & CANTAO, M.E.', 
-'Version: 0.0.1', 'E-mail: hanielcedraz@gmail.com', sep = "\n", collapse = '\n')))
+'Version: 0.0.2', 'E-mail: hanielcedraz@gmail.com', sep = "\n", collapse = '\n')))
 
 
 if ( !file.exists(opt$samplesFile) ) {
@@ -62,25 +59,30 @@ if (detectCores() < opt$procs){
     paste('Using ', detectCores(), 'threads')
 }
 
+# creating extracted_Folder
+extracted_Folder <- opt$extractedFolder
+if(!file.exists(file.path(extracted_Folder))) dir.create(file.path(extracted_Folder), recursive = TRUE, showWarnings = FALSE)
 
 ####################
 ### GENOME GENERATE
 ####################
 
-# index_Folder <- opt$genomeDir
-# if(!file.exists(file.path(index_Folder))) dir.create(file.path(index_Folder), recursive = TRUE, showWarnings = FALSE);
-
 star.index.function <- function(){
-    index_Folder <- paste(opt$genomeDir, '/', 'index_STAR', '/', sep = '')
+    index_Folder <- paste(dirname(opt$gtfTarget), '/', 'index_STAR', '/', sep = '')
     if(!file.exists(file.path(paste(index_Folder, '/', 'Genome', sep = '')))){ dir.create(file.path(index_Folder), recursive = TRUE, showWarnings = FALSE)
         procs <- ifelse(detectCores() < opt$procs, detectCores(), paste(opt$procs));
         PE <-paste()
         argments_index <- c('--runMode', 'genomeGenerate', '--runThreadN', procs, '--genomeDir', index_Folder, '--genomeFastaFiles', opt$mappingTarget, '--sjdbGTFfile', opt$gtfTarget, '--sjdbOverhang', opt$annoJunction-1)
-        system2('STAR', args = argments_index)
+        system2('/Users/haniel/Documents/BAQCOM/STAR_mac_2.7.0e', args = argments_index)
         
     } 
 }
 index_genom <- star.index.function()
+
+if (!file.exists(paste(dirname(opt$gtfTarget), '/', 'index_STAR', '/', 'Genome', sep = ''))) {
+    write(paste("Genome file does not exist\n"), stderr())
+    stop()
+}
 
 # Mapping analysis function
 mapping.STAR.function <- function(){
@@ -95,24 +97,17 @@ mapping.STAR.function <- function(){
         samtype <- paste('--outSAMtype', 'BAM Unsorted', 'SortedByCoordinate', sep = ' ')
         quantMode <- paste('--quantMode', 'TranscriptomeSAM', 'GeneCounts', sep = ' ')
         gzip <- paste('--readFilesCommand', opt$Uncompress, '-c', sep = ' ' )
-        argments_mapping <- c('--genomeDir', paste(opt$genomeDir, '/', 'index_STAR', '/', sep = ''), '--runThreadN', procs, gzip, '--readFilesIn', input_read1, input_read2, samtype, quantMode, '--outReadsUnmapped', 'Fastx', '--outFileNamePrefix', output_sample)
+        argments_mapping <- c('--genomeDir', paste(dirname(opt$gtfTarget), '/', 'index_STAR', '/', sep = ''), '--runThreadN', procs, gzip, '--readFilesIn', input_read1, input_read2, samtype, quantMode, '--outReadsUnmapped', 'Fastx', '--outFileNamePrefix', output_sample)
         
         system2('STAR', args = argments_mapping)
         
     }
     
 }
-
 mapping_genom <- mapping.STAR.function()
 
-
-# creating unmapped_Folder
-extracted_Folder <- opt$extractedFolder
-if(!file.exists(file.path(extracted_Folder))) dir.create(file.path(extracted_Folder), recursive = TRUE, showWarnings = FALSE)
-
-
-# Moving all unmapped files from 02-mappingSTAR folder to 03-Unmapped folder
-system('mv 02-mappingSTAR/*Unmapped.out.mate* 03-Unmapped/')
+# Moving all unmapped files from 02-mappingSTAR folder to 03-Ummapped folder
+system('mv 02-mappingSTAR/*Unmapped.out.mate* 03-Ummapped/')
 
 #Creating mapping report
 Final_Folder <- opt$mappingFolder
