d4b966cf8423182525a77a9b3ddd5b6d6437afed
hanielcedraz <30409702+hanielcedraz@users.noreply.github.com>
2019-03-22 16:36:28 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,43 +2,81 @@
 
 if [[ $1 == "-h" ]];
     then
-        echo "#Use this script to change the trimmomatic path in ".baqcom_qc", configure files, 
+        echo "#Use this script to change the trimmomatic path in ".baqcom_qc", configure files,
             and put theses file into the bash. More information read LEIAME.txt"
         exit 0
 elif [[ $# > 0 ]];
     then
         echo "It is not a valid argment. Try ./install.sh -h"
-        exit 1  
+        exit 1
 fi
 
-grep "$PWD" ~/.bash_profile > /dev/null
-if [ $? -ne 0 ]; 
+
+grep "$PWD" ~/.bash_profile ~/.bashrc > /dev/null
+if [ $? -ne 0 ];
     then
-        cat "baqcom_qc" | sed "s|XXX|$PWD|" > baqcom_qc.R
+      if [ "$(uname)" == "Linux" ]; then
+        echo -e "\n#Added by BAQCOM\nPATH=\$PATH:$PWD:\n" >> ~/.bashrc;
+        bash;
+        echo "Path" $PWD "added in ~/.bashrc successfully"
+      else
         echo -e "\n#Added by BAQCOM\nPATH=\$PATH:$PWD:\n" >> ~/.bash_profile;
         source ~/.bash_profile;
-        chmod +x baqcom_qc.R baqcom_mapping.R install_packages.R create_samples.sh run_test.sh ;
-        ./install_packages.R;
-        
+        echo "Path" $PWD "added in ~/.bash_profile successfully"
+    fi
+      else
+        if [ "$(uname)" == "Linux" ]; then
+        echo "Path" $PWD "It is already in ~/.bashrc"
+      else
+        echo "Path" $PWD "It is already in ~/.bash_profile"
+      fi
+fi
+
+
+if [ "$(uname)" == "Linux" ];
+    then
+      chmod +x bin/STAR_linux_2.7.0e
+      rm -f STAR
+      ln -s bin/STAR_linux_2.7.0e STAR
     else
-        echo "It is already installed"
+      chmod +x bin/STAR_mac_2.7.0e
+      rm -f STAR
+      ln -s bin/STAR_mac_2.7.0e STAR
 fi
 
- 
-if [ "$(uname)" == "Linux" ]; then
-     chmod +x bin/STAR_linux_2.7.0e
-     ln -s bin/STAR_linux_2.7.0e STAR
-     echo "Using Linux"
-else
-    chmod +x bin/STAR_mac_2.7.0e
-    ln -s bin/STAR_mac_2.7.0e STAR
-    echo "Using MacOS"
-fi
-unzip -q bin/fastqc_v0.11.8.zip -d bin
-rm -f bin/fastqc_v0.11.8.zip
-ln -s bin/FastQC/fastqc .
 
-mv baqcom_qc .baqcom_qc
-mv install_packages.R .install_packages.R
-chmod +x bin/FastQC/*.jar fastqc
-echo "successfully installed"
+
+if [[  -f baqcom_qc ]];
+    then
+        cat "baqcom_qc" | sed "s|XXX|$PWD|" > baqcom_qc.R
+        chmod +x baqcom_qc.R baqcom_mapping.R install_packages.R create_samples.sh run_test.sh;
+        rm baqcom_qc
+    else
+        echo "baqcom_qc.R already exists"
+fi
+
+if [[ -e bin/fastqc_v0.11.8.zip ]];
+   then
+       unzip -q -u bin/fastqc_v0.11.8.zip -d bin
+       mv -f bin/fastqc_v0.11.8.zip bin/.fastqc_v0.11.8.zip
+       rm -f fastqc
+       ln -s bin/FastQC/fastqc
+       chmod +x fastqc
+       echo "Symbolic link (fastqc) created successfully"
+   else
+       echo "fastqc (Symbolic link) already exists"
+
+fi
+
+
+if [ -e install_packages.R ];
+    then
+        ./install_packages.R;
+        mv install_packages.R .install_packages.R
+        echo "packages installed successfully"
+    else
+        echo "packages are already installed"
+fi
+
+
+#echo "successfully installed"
