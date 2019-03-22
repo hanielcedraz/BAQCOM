97bf409d1310d336b806caf1556b1ec87e4c08e8
hanielcedraz <30409702+hanielcedraz@users.noreply.github.com>
2019-03-22 08:48:13 -0300

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -18,11 +18,11 @@ echo -e "\n"
 echo -e "\n"
 echo "Running Quality Control Analysis"
 echo -e "\n"
-./baqcom_qc.R -p 36
+./baqcom_qc.R -p 36 -s 3 -l yes
 echo -e "\n"
 
 #Run baqcom_mapping.R
 echo -e "\n"
 echo "Running Mapping Analysis"
 echo -e "\n"
-./baqcom_mapping.R -p 20 -t examples/genome/Escherichia_coli.HUSEC2011CHR1.dna.toplevel.fa -g examples/genome/Escherichia_coli.HUSEC2011CHR1.42.gtf
+./baqcom_mapping.R -p 20 -q 3 -t examples/genome/Escherichia_coli.HUSEC2011CHR1.dna.toplevel.fa -g examples/genome/Escherichia_coli.HUSEC2011CHR1.42.gtf
