a55a8ede65d940ccc7e2ef08b6ef84279c40062b
hanielcedraz <30409702+hanielcedraz@users.noreply.github.com>
2019-03-29 16:03:41 -0300

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,4 +1,4 @@
-gunzip examples/genome/Escherichia*
+bunzip2 examples/genome/Sus_scrofa*
 echo -e "files extracted successfully\n"
 
 #create input_folder
@@ -18,11 +18,11 @@ echo -e "\n"
 echo -e "\n"
 echo "Running Quality Control Analysis"
 echo -e "\n"
-./baqcom_qc.R -p 36 -s 3 -l yes
+./baqcom_qc.R -p 36 -s 2 -l yes
 echo -e "\n"
 
 #Run baqcom_mapping.R
 echo -e "\n"
 echo "Running Mapping Analysis"
 echo -e "\n"
-./baqcom_mapping.R -p 20 -q 3 -t examples/genome/Escherichia_coli.HUSEC2011CHR1.dna.toplevel.fa -g examples/genome/Escherichia_coli.HUSEC2011CHR1.42.gtf
+./baqcom_mapping.R -p 20 -q 2 -t examples/genome/Sus_scrofa.Sscrofa11.1.dna.chromosome.1.fa -g examples/genome/Sus_scrofa.Sscrofa11.1.95.chromosome.1.gtf
