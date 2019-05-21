869aac36a40c94c23d29bf5b7b5ec0419c53e988
hanielcedraz <30409702+hanielcedraz@users.noreply.github.com>
2019-05-21 19:03:37 -0300

    Version 0.2.4

--- baqcom_mapping.R
+++ baqcom_mapping.R
@@ -76,11 +76,12 @@ if(casefold(opt$multiqc, upper = FALSE) == 'yes'){
 }
 
 
-if (!(opt$stranded %in% c("reverse", "yes", "no")))
-  cat('\n')
-  #write(paste('May have a mistake with the argument in -s parameter. Please verify if the argument is written in the right way'), stderr())
-stop(paste('May have a mistake with the argument in -s parameter. Please verify if the argument is written in the right way'))
 
+if (!(opt$stranded %in% c("reverse", "yes", "no"))){
+cat('\n')
+    write(paste('May have a mistake with the argument in -s parameter. Please verify if the argument is written in the right way'), stderr())
+stop()
+}
 
 
 #cat('\n')
