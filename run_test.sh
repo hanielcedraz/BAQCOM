63241ca9fe0651504fff361b88d764248cf09afb
hanielcedraz <30409702+hanielcedraz@users.noreply.github.com>
2019-06-27 10:50:20 +0200

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -18,11 +18,11 @@ echo -e "\n"
 echo -e "\n"
 echo "Running Quality Control Analysis"
 echo -e "\n"
-./baqcom_qc.R -p 36 -s 2 -l -r
+./baqcom_qc.R -p 36 -s 2 -l yes -r yes
 echo -e "\n"
 
 #Run baqcom_mapping.R
 echo -e "\n"
 echo "Running Mapping Analysis"
 echo -e "\n"
-./baqcom_mapping.R -p 20 -t examples/genome/Sus.Scrofa.chr1.genome.dna.toplevel.fa -g examples/genome/Sus.Scrofa.chr1.gene.annotation.gtf -m
+./baqcom_mapping.R -p 20 -t examples/genome/Sus.Scrofa.chr1.genome.dna.toplevel.fa -g examples/genome/Sus.Scrofa.chr1.gene.annotation.gtf -m yes
