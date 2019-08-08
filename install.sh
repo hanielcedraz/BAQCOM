f5a4e935fd4b7944c66bb0f0d013a9682052413a
hanielcedraz <30409702+hanielcedraz@users.noreply.github.com>
2019-08-08 10:01:05 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,116 +1,171 @@
 #!/bin/bash
 
+
+#Help menu
 if [[ $1 == "-h" ]];
     then
-        echo -e "\nUse this script to configurate the pipelines, install required softwares, configure files, and put theses files into the bash. More information read LEIAME.txt\n"
+        echo -e "\nUse this script to configurate the pipelines, install required softwares, configure files, and put theses files into the bash. More information read LEIAME.txt"
         exit 0
 elif [[ $# > 0 ]];
     then
-        echo "It is not a valid argment. Try ./install.sh -h"
+        echo -e "\nIt is not a valid argment. Try ./install.sh -h"
         exit 1
 fi
 
+#echo -e "\033[1m" #bols text
+#echo -e "\033[1;31m" #red text
 
+#Configuring path
 grep "$PWD" ~/.bash_profile ~/.bashrc > /dev/null
 if [ $? -ne 0 ];
     then
       if [ "$(uname)" == "Linux" ]; then
         echo -e "\n#Added by BAQCOM\nPATH=\$PATH:$PWD:\n" >> ~/.bashrc;
         #bash;
-        echo "Path" $PWD "added in ~/.bashrc successfully"
+        echo -e "\nPath" $PWD "added in ~/.bashrc successfully"
       else
         echo -e "\n#Added by BAQCOM\nPATH=\$PATH:$PWD:\n" >> ~/.bash_profile;
         #source ~/.bash_profile;
-        echo "Path" $PWD "added in ~/.bash_profile successfully"
+        echo -e "\nPath" $PWD "added in ~/.bash_profile successfully"
     fi
       else
         if [ "$(uname)" == "Linux" ]; then
-        echo "Path" $PWD "It is already in ~/.bashrc"
+        echo -e "\nPath" $PWD "It is already in ~/.bashrc"
       else
-        echo "Path" $PWD "It is already in ~/.bash_profile"
+        echo -e "\nPath" $PWD "It is already in ~/.bash_profile"
       fi
 fi
 
 
+#Configuring STAR, downloading Hisat2 and FeaturesCount in unix systems
 if [ "$(uname)" == "Linux" ];
     then
       chmod +x bin/STAR_linux_2.7.1a
       rm -f STAR
       ln -s bin/STAR_linux_2.7.1a STAR
-      echo "STAR symbolic link created successfully"
+      echo -e "\nSTAR symbolic link created successfully"
        if ! [ -d bin/hisat2-2.1.0 ];
         then
-              wget http://ccb.jhu.edu/software/hisat2/dl/hisat2-2.1.0-Linux_x86_64.zip -O bin/hisat2-2.1.0-Linux_x86_64.zip
-              cd bin/
-              unzip -q hisat2-2.1.0-Linux_x86_64.zip
-              cd ..
-              ln -s bin/hisat2-2.1.0/hisat2 hisat2
-              ln -s bin/hisat2-2.1.0/hisat2-build hisat2-build
-              ln -s bin/hisat2-2.1.0/hisat2_extract_exons.py
-              ln -s bin/hisat2-2.1.0/hisat2_extract_splice_sites.py
-              echo "hisat2 downloaded and symbolic link created successfully"
+            while true;
+            do
+              read -p "hisat2 doesn't exist in BAQCOM folder. Would you like to download it? [y,n] " dohisat
+              case $dohisat in
+                y|Y|yes|YES)
+                  wget http://ccb.jhu.edu/software/hisat2/dl/hisat2-2.1.0-Linux_x86_64.zip -O bin/hisat2-2.1.0-Linux_x86_64.zip
+                  cd bin/
+                  unzip -q hisat2-2.1.0-Linux_x86_64.zip
+                  cd ..
+                  ln -s bin/hisat2-2.1.0/hisat2 hisat2
+                  ln -s bin/hisat2-2.1.0/hisat2-build hisat2-build
+                  ln -s bin/hisat2-2.1.0/hisat2_extract_exons.py
+                  ln -s bin/hisat2-2.1.0/hisat2_extract_splice_sites.py
+                  echo -e "\nhisat2 downloaded and symbolic link created successfully"
+                  break;;
+                n|N|no|NO)
+                    echo -e "\nOk, I wont do that\n"
+                    break;;
+                *) echo "Insvalid option, specify y or n "
+              esac
+            done    
         else
-             echo "hisat2 exists and doesn't need to download"
+             echo -e "\nhisat2 exists and doesn't need to download"
         fi
         if ! [ -d bin/subread-1.6.4-source ];
         then
-            wget https://sourceforge.net/projects/subread/files/subread-1.6.4/subread-1.6.4-source.tar.gz -O bin/subread-1.6.4-source.tar.gz
-            cd bin/
-            tar xzf subread-1.6.4-source.tar.gz
-            cd subread-1.6.4-source/src
-            make -f Makefile.Linux
-            cd ../../../
-            ln -s bin/subread-1.6.4-source/bin/featureCounts featureCounts
+            while true;
+            do
+                read -p "featureCounts doesn't exist in BAQCOM folder. Would you like to download it? [y,n] " doft
+                case $doft in
+                  y|Y|yes|YES)
+                    wget https://sourceforge.net/projects/subread/files/subread-1.6.4/subread-1.6.4-source.tar.gz -O bin/subread-1.6.4-source.tar.gz
+                    cd bin/
+                    tar xzf subread-1.6.4-source.tar.gz
+                    cd subread-1.6.4-source/src
+                    make -f Makefile.Linux
+                    cd ../../../
+                    ln -s bin/subread-1.6.4-source/bin/featureCounts featureCounts
+                    break;;
+                  n|N|no|NO)
+                      echo -e "\nOk, I wont do that\n"
+                      break;;
+                  *) echo "Insvalid option, specify y or n "
+              esac
+            done
         else
-            echo "featureCounts exists and doesn't need to download"
+            echo -e "\nfeatureCounts exists and doesn't need to download"
         fi
     else
       chmod +x bin/STAR_mac_2.7.1a
       rm -f STAR
       ln -s bin/STAR_mac_2.7.1a STAR
-      echo "STAR symbolic link created successfully"
+      echo -e "\nSTAR symbolic link created successfully"
         if ! [ -d bin/hisat2-2.1.0 ];
         then
-              wget http://ccb.jhu.edu/software/hisat2/dl/hisat2-2.1.0-OSX_x86_64.zip -O bin/hisat2-2.1.0-OSX_x86_64.zip
-              cd bin/
-              unzip -q hisat2-2.1.0-OSX_x86_64.zip
-              cd ..
-              ln -s bin/hisat2-2.1.0/hisat2 hisat2
-              ln -s bin/hisat2-2.1.0/hisat2-build hisat2-build
-              ln -s bin/hisat2-2.1.0/hisat2_extract_exons.py
-              ln -s bin/hisat2-2.1.0/hisat2_extract_splice_sites.py
-              echo "hisat2 downloaded and symbolic link created successfully"
+            while true;
+            do
+              read -p "hisat2 doesn't exist in BAQCOM folder. Would you like to download it? [y,n] " dohisat
+              case $dohisat in
+                y|Y|yes|YES)
+                  wget http://ccb.jhu.edu/software/hisat2/dl/hisat2-2.1.0-OSX_x86_64.zip -O bin/hisat2-2.1.0-OSX_x86_64.zip
+                  cd bin/
+                  unzip -q hisat2-2.1.0-OSX_x86_64.zip
+                  cd ..
+                  ln -s bin/hisat2-2.1.0/hisat2 hisat2
+                  ln -s bin/hisat2-2.1.0/hisat2-build hisat2-build
+                  ln -s bin/hisat2-2.1.0/hisat2_extract_exons.py
+                  ln -s bin/hisat2-2.1.0/hisat2_extract_splice_sites.py
+                  echo -e "\nhisat2 downloaded and symbolic link created successfully\n"
+                  break;;
+                n|N|no|NO)
+                  echo -e "\nOk, I wont do that\n"
+                  break;;
+                *) echo "Insvalid option, specify y or n "
+              esac
+            done
         else
-             echo "hisat2 exists and doesn't need to download"
+             echo -e "\nhisat2 exists and doesn't need to download"
         fi
         if ! [ -d bin/subread-1.6.4-source ];
         then
-            wget https://sourceforge.net/projects/subread/files/subread-1.6.4/subread-1.6.4-source.tar.gz -O bin/subread-1.6.4-source.tar.gz
-            cd bin/
-            tar xzf subread-1.6.4-source.tar.gz
-            cd subread-1.6.4-source/src
-            make -f Makefile.MacOS
-            cd ../../../
-            ln -s bin/subread-1.6.4-source/bin/featureCounts featureCounts
+            while true;
+            do
+                read -p "featureCounts doesn't exist in BAQCOM folder. Would you like to download it? [y,n] " doft
+                case $doft in
+                  y|Y|yes|YES)
+                    wget https://sourceforge.net/projects/subread/files/subread-1.6.4/subread-1.6.4-source.tar.gz -O bin/subread-1.6.4-source.tar.gz
+                    cd bin/
+                    tar xzf subread-1.6.4-source.tar.gz
+                    cd subread-1.6.4-source/src
+                    make -f Makefile.MacOS
+                    cd ../../../
+                    ln -s bin/subread-1.6.4-source/bin/featureCounts featureCounts
+                    break;;
+                  n|N|no|NO)
+                    echo -e "\nOk, I wont do that\n"
+                    break;;
+                  *) echo "Insvalid option, specify y or n  "
+                esac
+            done
         else
-            echo "featureCounts exists and doesn't need to download"
+            echo -e "\nfeatureCounts exists and doesn't need to download"
         fi
 fi
 
 
-
+#Configuring trimmomatic pipeline
 if [[  -f baqcomTrimmomatic ]];
     then
         cat "baqcomTrimmomatic" | sed "s|XXX|$PWD|" > baqcomTrimmomatic.R
         chmod +x baqcomTrimmomatic.R;
         rm baqcomTrimmomatic
     else
-        echo "baqcomTrimmomatic.R already exists"
+        echo -e "\nbaqcomTrimmomatic.R already exists"
 fi
 
-
+#giving executable permission to the pipelines and scripts
       chmod +x baqcomFeaturesCount.R baqcomHisat2Mapping.R baqcomHtseqCounting.R baqcomSTARmapping.R createSamples.sh runTest.sh countReads.R
 
+#install and configure fastQC
 if [[ -e bin/fastqc_v0.11.8.zip ]];
    then
        unzip -q -u bin/fastqc_v0.11.8.zip -d bin
@@ -118,21 +173,21 @@ if [[ -e bin/fastqc_v0.11.8.zip ]];
        rm -f fastqc
        ln -s bin/FastQC/fastqc
        chmod +x fastqc
-       echo "Symbolic link (fastqc) created successfully"
+       echo -e "\nSymbolic link (fastqc) created successfully"
    else
-       echo "fastqc (Symbolic link) already exists"
+       echo -e "\nfastqc (Symbolic link) already exists"
 
 fi
 
-
+#Install required R packages
 if [ -e installPackages.R ];
     then
         chmod +x installPackages.R;
         ./installPackages.R;
         mv installPackages.R .installPackages.R
-        echo "packages installed successfully"
+        echo -e "\npackages installed successfully"
     else
-        echo "packages are already installed"
+        echo -e "\npackages are already installed\n"
 fi
 
 
@@ -143,3 +198,4 @@ if [ "$(uname)" == "Linux" ];
         source ~/.bash_profile;
 fi
 #echo "successfully installed"
+#echo -e "\033[0m" #Regular color
