c8042a6f8781700ed8147c2cc96d6893f9a680e8
hanielcedraz <30409702+hanielcedraz@users.noreply.github.com>
2019-09-04 10:51:48 +0200

    Updating STAR version

--- install.sh
+++ install.sh
@@ -40,9 +40,9 @@ fi
 #Configuring STAR, downloading Hisat2 and FeaturesCount in unix systems
 if [ "$(uname)" == "Linux" ];
     then
-      chmod +x bin/STAR_linux_2.7.2a
+      chmod +x bin/STAR_linux_2.7.2b
       rm -f STAR
-      ln -s bin/STAR_linux_2.7.2a STAR
+      ln -s bin/STAR_linux_2.7.2b STAR
       echo -e "\nSTAR symbolic link created successfully"
        if ! [ -d bin/hisat2-2.1.0 ];
         then
@@ -95,9 +95,9 @@ if [ "$(uname)" == "Linux" ];
             echo -e "\nfeatureCounts exists and doesn't need to download"
         fi
     else
-      chmod +x bin/STAR_mac_2.7.2a
+      chmod +x bin/STAR_mac_2.7.2b
       rm -f STAR
-      ln -s bin/STAR_mac_2.7.2a STAR
+      ln -s bin/STAR_mac_2.7.2b STAR
       echo -e "\nSTAR symbolic link created successfully"
         if ! [ -d bin/hisat2-2.1.0 ];
         then
