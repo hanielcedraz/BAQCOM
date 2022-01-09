c2e1b1bef2506e2147dd6952d81fba4ddc267f52
hanielcedraz <30409702+hanielcedraz@users.noreply.github.com>
2022-01-09 10:19:50 -0300

    Update installPackages.R

--- installPackages.R
+++ installPackages.R
@@ -6,16 +6,29 @@ Install_Multiples_Packages <- function(packages) {
   if (length(pack)) {
     install.packages(pack, repos = 'https://cran.rstudio.com/', Ncpus = 8)
   }
-
+  
   for (package_i in packages) {
     suppressPackageStartupMessages(library(package_i, character.only = TRUE, quietly = TRUE))
-    }
-
+  }
+  
 }
 
-Install_Multiples_Packages(c('optparse', 'parallel', 'tools', 'dplyr', 'data.table', 'glue'))
+Install_Multiples_Packages(c("optparse", "parallel", "tools", "dplyr", "data.table", "glue", "devtools"))
 
 
+if (!"baqcomPackage" %in% installed.packages()[,'Package']) {
+  devtools::install_github(repo = "git@github.com:hanielcedraz/baqcomPackage.git", upgrade = "never", quiet = TRUE, force = TRUE)
+  suppressPackageStartupMessages(library(baqcomPackage))
+}
 
 
+newPackages <- c("optparse", "parallel", "tools", "dplyr", "data.table", "glue", "devtools", "baqcomPackage")
+newInstalledPackages <- newPackages[(newPackages %in% installed.packages()[,'Package'])];
+
+if (length(newInstalledPackages) == 8) {
+  write(paste("All packages installed successfully."), stdout())
+  
+} else if (length(newInstalledPackages) < 8) {
+  write(paste("Could not install", paste0(newInstalledPackages, collapse = ", "), "-", "Please install it manually."), stdout())
+}
 
